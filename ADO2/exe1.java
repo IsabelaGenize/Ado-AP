431f69fa3d99baa0e7737ab175441dc5df46724c
NicolasAndrade1605 <87158638+NicolasAndrade1605@users.noreply.github.com>
2023-09-30 20:32:56 -0300

    primeiros exercicios

---
+++ ADO2/exe1.java
@@ -0,0 +1,16 @@
+package ADO2;
+
+//Faça um programa que mostre todos os números inteiros de 1 a 50. Use o laço WHILE
+
+public class exe1 {
+
+    public static void main(String[] args) {
+        
+        int numero = 1;
+
+        while (numero <= 50) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
---
+++ ADO2/exe2.java
@@ -0,0 +1,14 @@
+package ADO2;
+
+import java.util.Scanner;
+
+//Faça um programa que leia um número N do usuário, some todos os números inteiros de 1 a N, e mostre o resultado obtido. Use o laço WHILE
+
+public class exe2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+}
