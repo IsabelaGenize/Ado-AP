e9d71f5f138ef33471ef924cfd07dd749c8b3f80
IsabelaGenize <isabeladupre@gmail.com>
2023-10-01 17:07:50 -0300

    exe20

--- ADO2/exe20.java
+++ ADO2/exe20.java
@@ -6,49 +6,44 @@ public class exe20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar o saldo inicial
-        System.out.print("Digite o saldo inicial da conta: ");
+        System.out.print("Digite aqui o saldo inicial da conta: ");
         double saldo = scanner.nextDouble();
 
         int opcao;
-        double valor;
+        double dinheiro;
 
         do {
-            // Exibir opções ao usuário
             System.out.println("\nOpções:");
-            System.out.println("1. Realizar depósito");
-            System.out.println("2. Realizar saque");
-            System.out.println("0. Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.println("1. Realizar um depósito");
+            System.out.println("2. Realizar um saque");
+            System.out.println("3. Sair");
+            System.out.print("Escolha a opção desejada: ");
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    // Realizar depósito
-                    System.out.print("Digite o valor do depósito: ");
-                    valor = scanner.nextDouble();
-                    saldo += valor;
-                    System.out.println("Depósito de R$" + valor + " realizado. Saldo atual: R$" + saldo);
+                    System.out.print("Digite o valor que deseja depositar: ");
+                    dinheiro = scanner.nextDouble();
+                    saldo += dinheiro;
+                    System.out.println("Depósito de R$" + dinheiro + " foi feito com sucesso. Seu saldo atual é de:  R$" + saldo);
                     break;
                 case 2:
-                    // Realizar saque
                     System.out.print("Digite o valor do saque: ");
-                    valor = scanner.nextDouble();
-                    if (valor <= saldo) {
-                        saldo -= valor;
-                        System.out.println("Saque de R$" + valor + " realizado. Saldo atual: R$" + saldo);
+                    dinheiro = scanner.nextDouble();
+                    if (dinheiro <= saldo) {
+                        saldo -= dinheiro;
+                        System.out.println("Saque de R$" + dinheiro + " feito com sucesso. Seu saldo atual é de:  R$" + saldo);
                     } else {
-                        System.out.println("Saldo insuficiente para realizar o saque.");
+                        System.out.println("Seu saldo é insuficiente para realizar a operação.");
                     }
                     break;
-                case 0:
-                    // Sair do programa
-                    System.out.println("Encerrando o programa.");
+                case 3:
+                    System.out.println("Operação encerrada, obrigada!");
                     break;
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("Opção inválida, por favor tente novamente!");
             }
-        } while (opcao != 0);
+        } while (opcao != 3);
 
         scanner.close();
     }
