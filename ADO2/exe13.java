ed6627951ad468c826f1ed6673dbc756a40cd1ca
NicolasAndrade1605 <87158638+NicolasAndrade1605@users.noreply.github.com>
2023-10-02 14:29:42 -0300

    update

--- ADO2/exe13.java
+++ ADO2/exe13.java
@@ -1,5 +1,30 @@
 package ADO2;
 
+import java.util.Scanner;
+
+//Faça um programa que leia um número natural N e calcule a soma abaixo (lembre-se de que tanto as divisões quanto o resultado devem ser decimais). Utilize o laço que lhe for mais conveniente.
+// S = 1 + (1*2)/(1*3) + (1*2*3)/(1*3*5) + (1*2*3*4) + (1*3*5*7) + ... + (1*2*3*...*n)/1*3*5*...*(2n - 1)
+
 public class exe13 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int n = entrada.nextInt();
+
+        double soma = 0.0;
+        double conta = 1.0;
+
+        for(int i = 1; i <= n; i++){
+            double numero = 1.0;
+            for(int j = 1; j <= i; j++){
+                numero *= j;
+            }
+
+            soma += numero / conta;
+            conta *= (2 * i + 1);
+        }
+
+        System.out.println("O resultado da soma da série é: " + soma);
+    }
 }
--- ADO2/exe14.java
+++ ADO2/exe14.java
@@ -1,5 +1,23 @@
 package ADO2;
 
+import java.util.Scanner;
+
+//Faça um programa que leia um número natural N e calcule a soma abaixo (lembre-se de que tanto as divisões quanto o resultado devem ser decimais). Utilize o laço que lhe for mais conveniente.
+// S = 1 + 2/3 + 4/5 + 6/7 + ... + 2n/(2n + 1)
+
 public class exe14 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int n = entrada.nextInt();
+
+        double soma = 0.0;
+
+        for (int i = 1; i <= n; i++){
+            soma += (double)(2*i) / (2*i+1);
+        }
+
+        System.out.println("O resultado da soma da série é: " + soma);
+    }
 }
